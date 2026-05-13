8ef024c0b11c4d218ff8182287e66899c504832d
alfiannurrizky <alfiannrzky0@gmail.com>
2026-05-13 13:15:19 +0700

    fix endpoint

--- backend/init.sql
+++ backend/init.sql
@@ -3,7 +3,7 @@ USE vuln_db;
 
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
+  username VARCHAR(255) NOT NULL,
   email VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   role VARCHAR(20) DEFAULT 'user',
